44680e6365fcb3c5b90aadcf65981266bcdbf7e2
Mariusz Gęzicki <mariusz.v7@gmail.com>
2018-02-13 15:36:35 +0100

    Display simple circle as proof of concept

--- src/map-view-port.ts
+++ src/map-view-port.ts
@@ -1,15 +1,39 @@
+import { select, Selection, BaseType } from 'd3-selection';
+
 export class MapViewPort extends HTMLElement {
     private shadow: ShadowRoot;
+    private container: HTMLElement;
+    private d3root: Selection<BaseType, any, any, any>;
+    private width = 0;
+    private height = 0;
 
     constructor() {
         super();
 
         this.shadow = this.attachShadow({ mode: 'open' });
+        this.container = document.createElement('div');
 
-        const child = document.createElement('p');
-        child.textContent = 'Hello custom element!';
+        this.shadow.appendChild(this.container);
+    }
 
-        this.shadow.appendChild(child);
+    private connectedCallback() {
+        this.height = parseInt(this.getAttribute('height') || '0', 10);
+        this.width = parseInt(this.getAttribute('width') || '0', 10);
+
+        this.d3root = select(this.container).append('svg')
+            .attr('width', this.width)
+            .attr('height', this.height);
+
+        this.render();
+    }
+
+    private render() {
+
+        this.d3root.append('circle')
+            .attr('cx', 30)
+            .attr('cy', 30)
+            .attr('r', 10);
+
     }
 
 }
