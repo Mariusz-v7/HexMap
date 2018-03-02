e9b50b889329408cb7fb363b0fc091e1f3183beb
Mariusz Gęzicki <mariusz.v7@gmail.com>
2018-03-02 12:59:31 +0100

    Prepare map fragment controller

--- src/map-fragment/map-fragment-controller.ts
+++ src/map-fragment/map-fragment-controller.ts
@@ -1,8 +1,15 @@
 import { MapFragmentTile } from './map-fragment-tile';
-import { Selection, BaseType } from 'd3-selection';
+import { Selection, BaseType, select } from 'd3-selection';
 
 export class MapFragmentController {
-    constructor(private root: Selection<BaseType, any, any, any>) {
-        const mft = new MapFragmentTile(root);
+    private d3Root: Selection<HTMLElement, any, any, any>;
+
+    constructor(private container: HTMLElement) {
+        this.d3Root = select(this.container);
+        const width = this.container.clientWidth;
+        const height = this.container.clientHeight;
+
+        // todo: compute amount of tiles and their location
+        // todo: load and unload tiles dynamically when user drags the map
     }
 }
--- src/map-view-port.ts
+++ src/map-view-port.ts
@@ -8,16 +8,15 @@ import { MapFragmentController } from './map-fragment/map-fragment-controller';
 export class MapViewPort extends HTMLElement {
     private shadow: ShadowRoot;
     private container: HTMLElement;
-    private d3root: Selection<BaseType, any, any, any>;
     private width = 0;
     private height = 0;
-    private renderer: Renderer;
 
     constructor() {
         super();
 
         this.shadow = this.attachShadow({ mode: 'open' });
         this.container = document.createElement('div');
+        this.container.setAttribute('class', 'container');
 
         const style = document.createElement('style');
         style.textContent = styles;
@@ -27,25 +26,15 @@ export class MapViewPort extends HTMLElement {
     }
 
     private connectedCallback() {
-        // this.height = parseInt(this.getAttribute('height') || '0', 10);
-        // this.width = parseInt(this.getAttribute('width') || '0', 10);
+        this.height = parseInt(this.getAttribute('height') || '0', 10);
+        this.width = parseInt(this.getAttribute('width') || '0', 10);
 
-        this.d3root = select(this.container).append('svg')
-            .attr('width', this.width)
-            .attr('height', this.height);
+        this.container.setAttribute('style', `
+            width: ${this.width}px;
+            height: ${this.height}px;
+        `);
 
-        // const mapContainer = this.d3root.append('g');
-
-        // const topology = new Topology();
-
-        // this.renderer = new Renderer(this.d3root, mapContainer, topology);
-        // this.renderer.render();
-
-        // const mapDrag = new MapController(this.d3root, topology, this.renderer, this.width, this.height);
-
-        //tmp:
-
-        const mapFragmentController = new MapFragmentController(select(this.container));
+        const mapFragmentController = new MapFragmentController(this.container);
     }
 
 }
