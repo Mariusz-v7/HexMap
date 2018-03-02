f959dac237be48fec22da5a975ed4cc814d6d561
Mariusz Gęzicki <mariusz.v7@gmail.com>
2018-03-02 13:20:03 +0100

    Disable zooming, prepare panning

--- src/map-fragment/map-fragment-controller.ts
+++ src/map-fragment/map-fragment-controller.ts
@@ -1,13 +1,26 @@
 import { MapFragmentTile } from './map-fragment-tile';
-import { Selection, BaseType, select } from 'd3-selection';
+import { Selection, BaseType, select, event } from 'd3-selection';
+import { zoom } from 'd3-zoom';
 
 export class MapFragmentController {
     private d3Root: Selection<HTMLElement, any, any, any>;
+    private svg: Selection<BaseType, any, any, any>;
 
     constructor(private container: HTMLElement) {
         this.d3Root = select(this.container);
         const width = this.container.clientWidth;
         const height = this.container.clientHeight;
+
+        const zoomDef = zoom()
+            .scaleExtent([1, 1])
+            .on('zoom', () => {
+                console.log(event.transform)
+            });
+
+        this.svg = this.d3Root.append('svg')
+            .attr('width', width)
+            .attr('height', height)
+            .call(zoomDef);
 
         // todo: compute amount of tiles and their location
         // todo: load and unload tiles dynamically when user drags the map
